8b17adf85f6dafd2b9b1d257c2dd2087cc4fb83d
willianmontalbo <84750137+willianmontalbo@users.noreply.github.com>
2021-05-24 21:56:36 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Willian"
+    return "amigo vc nem eh um passaro, vc nao possui bico nem penas, muito menos asas, eu duvido que algum especialista da area da taxonomia um dia poderia considerar vc como um passaro, sinto muita pena da sua vida miseravel e como voce tirou a vida de meu amigo a sangue frio, não consigo sentir a minima empatia por um ser como voce, espero que vc repense seus conceitos e tome um bom rumo com a tua vida"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
